2856eb8281238f570cd040e5c0a4aaa23d73a511
Gurukalyan Mohanty <h20170162@pilani.bits-pilani.ac.in>
2018-04-27 14:18:23 +0530

    Final project

---
+++ USB-TO-I2C-CONV/kernel/.tmp_versions/usb_to_i2c.mod
@@ -0,0 +1,3 @@
+/home/guru/Desktop/USB-TO-I2C_Finall/kernel/usb_to_i2c.ko
+/home/guru/Desktop/USB-TO-I2C_Finall/kernel/usb_to_i2c.o
+
---
+++ USB-TO-I2C-CONV/kernel/.usb_to_i2c.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/guru/Desktop/USB-TO-I2C_Finall/kernel/usb_to_i2c.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/guru/Desktop/USB-TO-I2C_Finall/kernel/usb_to_i2c.ko /home/guru/Desktop/USB-TO-I2C_Finall/kernel/usb_to_i2c.o /home/guru/Desktop/USB-TO-I2C_Finall/kernel/usb_to_i2c.mod.o ;  true
